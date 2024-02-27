eedafe1963342ae26f4728dd2057bb11d3cb204d
Russell Feldhausen <russfeldh@gmail.com>
2024-02-27 09:43:41 -0600

    Python

---
+++ python/client/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+const users = ref(null);
+fetch('http://localhost:5001/users')
+    .then(response => response.json())
+    .then(data => users.value = data);
+</script>
+
+<template>
+  <ul>
+    <li v-for="user in users">{{ user.name }} ({{  user.eid }})</li>
+  </ul>
+</template>
